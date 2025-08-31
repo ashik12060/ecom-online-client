f444a55dd868728cad31126a04575c5817c2d611
Ashik Mahmood <wd.ashik04@gmail.com>
2025-08-31 19:16:17 +0600

    update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDsy_p4bNtdLZrMpiI-YPu4la62dqfXTXg",
-  authDomain: "phone-authentication-b5248.firebaseapp.com",
-  projectId: "phone-authentication-b5248",
-  storageBucket: "phone-authentication-b5248.appspot.com",
-  messagingSenderId: "916446005479",
-  appId: "1:916446005479:web:3d40761edcbf2a700dba42",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
